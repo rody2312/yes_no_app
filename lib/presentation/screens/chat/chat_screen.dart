7438a19652b5b7a6ea33a2057d4d7f47e0549209
rody2312 <84883025+rody2312@users.noreply.github.com>
2023-06-08 22:54:07 -0400

    envio de mensajes agregado

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -42,6 +42,7 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
+                controller: chatProvider.chatScrollController,
                 itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
                   final message = chatProvider.messageList[index];
@@ -53,7 +54,9 @@ class _ChatView extends StatelessWidget {
               )),
 
             ///Caja de texto de mensaje
-            const MessageFieldBox(),
+            MessageFieldBox(
+              onValue: chatProvider.sendMessage,
+            ),
           ],
         ),
       ),
